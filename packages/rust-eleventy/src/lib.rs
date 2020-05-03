45803d4243706e638206ffb43ec20f0272e249ad
Roman Gusev <rgusev2@gmail.com>
2020-05-03 13:20:47 +0600

    Add rust eleventy lib

---
+++ packages/rust-eleventy/src/lib.rs
@@ -0,0 +1,21 @@
+/// An absolutely useless function that alway returns 110.
+///
+/// eleven +‎ -ty, from Old English hundendleftig (also spelled hund-endleofantig,
+/// hund-endlyftig, and hund-ælleftig).
+///
+/// Originally derived from Tolkien's Lord of the Rings trilogy.
+/// Bilbo Baggin's eleventy-first birth day was the setting of the opening chapter.
+/// Since used by people all over the world.
+///
+/// ```
+/// let result = eleventy::eleventy();
+/// assert_eq!(result, 110);
+/// ```
+///
+/// ```
+/// let result = eleventy::eleventy();
+/// assert_ne!(result, 42);
+/// ```
+pub fn eleventy() -> u8 {
+    110
+}
